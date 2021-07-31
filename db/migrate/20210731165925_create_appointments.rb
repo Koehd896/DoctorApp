47dbcfa21c4c5de109a6a6aa8b6957468575c075
Raven <ravenkoehler@gmail.com>
2021-07-31 13:01:21 -0400

    created appointment model and controller

---
+++ db/migrate/20210731165925_create_appointments.rb
@@ -0,0 +1,11 @@
+class CreateAppointments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :appointments do |t|
+      t.integer :patient_id
+      t.integer :doctor_id
+      t.datetime :date
+
+      t.timestamps
+    end
+  end
+end
