a127d7a352b95c7b5f527ea6f8dd06fd66c3cb2f
Raven <ravenkoehler@gmail.com>
2021-08-01 14:42:41 -0400

    added new and create actions to doctor and patient controller

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,16 @@
 class DoctorsController < ApplicationController
+    def new
+        @doctor = Doctor.new
+    end
+
+    def create
+        @doctor = Doctor.create(doctor_params)
+        redirect_to doctor_path(@doctor)
+    end
+
+    private
+
+    def doctor_params
+        params.require(:doctor).permit(:name, :password)
+    end
 end
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,17 @@
 class PatientsController < ApplicationController
+    def new
+        @patient = Patient.new
+    end
+
+    def create
+        @patient = Patient.create(patient_params)
+        redirect_to patient_path(@patient)
+    end
+
+
+    private
+
+    def patient_params
+        params.require(:patient).permit(:name, :password)
+    end
 end
