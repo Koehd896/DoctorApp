1ad6c20d47dc1382b59b2b235bb52f214fe72fb2
Raven <ravenkoehler@gmail.com>
2021-08-06 13:18:57 -0400

    added nested routes for appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,15 @@
 class AppointmentsController < ApplicationController
+
+    def new
+        @appointment = Appointment.new
+    end
+
+    def create
+    end 
+
+    def index
+    end
+
+    def show
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     root 'static#home'
     
     resources :doctors, :patients
+    resources :patients, only: [:show] do
+        resources :appointments, only: [:new, :create]
+    end
     post '/sessions/destroy', to: 'sessions#destroy'
     post '/sessions/create', to: 'sessions#create'
     get '/patientlogin', to: 'patients#login'
