648177a2ca24e6c553c46fd070bdd2c0846c9d7c
Raven <ravenkoehler@gmail.com>
2021-08-06 13:43:48 -0400

    added date validation for appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,15 +1,27 @@
 class AppointmentsController < ApplicationController
 
     def new
-        @appointment = Appointment.new
+        @appointment = Appointment.new(patient_id: params[:patient_id])
     end
 
     def create
+        @appointment = Appointment.new(appointment_params)
+        if @appointment.save
+            redirect_to appointment_path(@appointment)
+        else
+            render :new
+        end
     end 
 
     def index
     end
 
     def show
+    end
+
+    private
+
+    def appointment_params
+        params.require(:appointment).permit(:date, :patient_id, :doctor_id)
     end
 end
