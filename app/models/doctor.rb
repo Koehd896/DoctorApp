0c6ad600316e2f81e94b27f331e41a38f1981249
Raven <ravenkoehler@gmail.com>
2021-08-31 18:14:25 -0400

    added distinct to doctor and patient models

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,7 +1,7 @@
 class Doctor < ApplicationRecord
     has_many :appointments
     has_many :prescriptions
-    has_many :patients, through: :appointments
+    has_many :patients, -> { distinct }, through: :appointments
 
     has_secure_password
 
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,7 +2,7 @@ class Patient < ApplicationRecord
     has_many :prescriptions
     has_many :medications, through: :prescriptions
     has_many :appointments
-    has_many :doctors, through: :appointments
+    has_many :doctors, -> { distinct }, through: :appointments
 
     has_secure_password
 
